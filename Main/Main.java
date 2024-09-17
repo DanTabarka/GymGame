305d9cc2adde73ebf0418c034308badff85d4e5b
DanTabarka <tabarka.dan@gmail.com>
2024-09-17 15:57:21 +0200

    first commit

---
+++ Main/Main.java
@@ -0,0 +1,18 @@
+package Main;
+
+import javax.swing.JFrame;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        JFrame window = new JFrame();
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        window.setResizable(false);
+        window.setTitle("GymGame");
+
+        window.setLocationRelativeTo(null);
+        window.setVisible(true);
+
+    }
+}
